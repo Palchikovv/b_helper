2b45763cd8ec4abb63ce905e55ffaf520537693d
Palchikovv <60886124+Palchikovv@users.noreply.github.com>
2020-03-21 10:25:34 +0300

    Update setting_1.js

--- setting_1.js
+++ setting_1.js
@@ -8,19 +8,19 @@
   GM_setValue('percentThreshold_DASHUSD', 70)
   
 
-  GM_setValue('dynLevelCoef_BTCUSD', 1.82)
-  GM_setValue('dynLevelCoef_ETHUSD', 1.84)
-  GM_setValue('dynLevelCoef_XRPUSD', 1.88)
-  GM_setValue('dynLevelCoef_ETCUSD', 1.88)
+  GM_setValue('dynLevelCoef_BTCUSD', 1.86)
+  GM_setValue('dynLevelCoef_ETHUSD', 1.88)
+  GM_setValue('dynLevelCoef_XRPUSD', 1.92)
+  GM_setValue('dynLevelCoef_ETCUSD', 1.98)
   GM_setValue('dynLevelCoef_ZECUSD', 1.99)
   GM_setValue('dynLevelCoef_DASHUSD', 1.96)
  
   GM_setValue('dynLevelSafe_BTCUSD', 52)
-  GM_setValue('dynLevelSafe_ETHUSD', 56)
-  GM_setValue('dynLevelSafe_XRPUSD', 58)
-  GM_setValue('dynLevelSafe_ETCUSD', 56)
+  GM_setValue('dynLevelSafe_ETHUSD', 52)
+  GM_setValue('dynLevelSafe_XRPUSD', 50)
+  GM_setValue('dynLevelSafe_ETCUSD', 50)
   GM_setValue('dynLevelSafe_ZECUSD', 59)
-  GM_setValue('dynLevelSafe_DASHUSD', 54)
+  GM_setValue('dynLevelSafe_DASHUSD', 56)
 
   GM_setValue('diffThreshold_BTCUSD', [-5, 9])
   GM_setValue('diffThreshold_ETHUSD', [-0.26, 0.2])
