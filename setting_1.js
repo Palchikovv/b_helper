501eaabbd1d39f9038e7c48b8b333d8fcf08c09c
Palchikovv <60886124+Palchikovv@users.noreply.github.com>
2020-03-29 09:04:58 +0300

    Update setting_1.js

--- setting_1.js
+++ setting_1.js
@@ -21,7 +21,7 @@
   GM_setValue('dynLevelSafe_XRPUSD', 62)
   GM_setValue('dynLevelSafe_ETCUSD', 60)
   GM_setValue('dynLevelSafe_ZECUSD', 59)
-  GM_setValue('dynLevelSafe_DASHUSD', 66)
+  GM_setValue('dynLevelSafe_DASHUSD', 64)
 
   GM_setValue('diffThreshold_BTCUSD', [-5, 9])
   GM_setValue('diffThreshold_ETHUSD', [-0.26, 0.2])
