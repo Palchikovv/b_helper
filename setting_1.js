7a7a8b2b0beb6ecb5d3990986172c7bcbd126741
Palchikovv <60886124+Palchikovv@users.noreply.github.com>
2020-03-31 22:31:19 +0300

    Update setting_1.js

--- setting_1.js
+++ setting_1.js
@@ -8,10 +8,10 @@
   GM_setValue('percentThreshold_DASHUSD', 70)
  
   
-    GM_setValue('dynLevelCoef_BTCUSD', 1.86)
+    GM_setValue('dynLevelCoef_BTCUSD', 1.84)
   GM_setValue('dynLevelCoef_ETHUSD', 1.88)
-  GM_setValue('dynLevelCoef_XRPUSD', 1.89)
-  GM_setValue('dynLevelCoef_ETCUSD', 1.86)
+  GM_setValue('dynLevelCoef_XRPUSD', 1.9)
+  GM_setValue('dynLevelCoef_ETCUSD', 1.88)
   GM_setValue('dynLevelCoef_ZECUSD', 1.99)
   GM_setValue('dynLevelCoef_DASHUSD', 1.96)
 
