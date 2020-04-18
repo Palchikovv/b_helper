4dfbb7857c18b6eb0478fa88c1ed668c118aa6db
Palchikovv <60886124+Palchikovv@users.noreply.github.com>
2020-04-18 12:49:49 +0300

    Update setting_1.js

--- setting_1.js
+++ setting_1.js
@@ -10,7 +10,7 @@
   
   GM_setValue('dynLevelCoef_BTCUSD', 1.92)
   GM_setValue('dynLevelCoef_ETHUSD', 1.9)
-  GM_setValue('dynLevelCoef_XRPUSD', 1.92)
+  GM_setValue('dynLevelCoef_XRPUSD', 1.9)
   GM_setValue('dynLevelCoef_ETCUSD', 1.9)
   GM_setValue('dynLevelCoef_ZECUSD', 1.96)
   GM_setValue('dynLevelCoef_DASHUSD', 1.96)
