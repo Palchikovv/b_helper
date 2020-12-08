7df76573a59ff16baf085b3df91f41b808df119a
Palchikovv <60886124+Palchikovv@users.noreply.github.com>
2020-12-08 19:36:00 +0300

    Update setting_1.js

--- setting_1.js
+++ setting_1.js
@@ -40,7 +40,7 @@ GM_setValue('devThresh_EURAUD', [-0.009, 0.008])
 GM_setValue('devThresh_CHFJPY', [-0.009, 0.008])
 GM_setValue('devThresh_USDCHF', [-0.009, 0.008])
 GM_setValue('devThresh_EURUSD', [-0.008, 0.007])
-GM_setValue('devThresh_GBPUSD', [-0.009, 0.008])
+GM_setValue('devThresh_GBPUSD', [-0.010, 0.009])
 GM_setValue('devThresh_AUDCHF', [-0.009, 0.008])
 GM_setValue('devThresh_USDCAD', [-0.009, 0.008])
 
